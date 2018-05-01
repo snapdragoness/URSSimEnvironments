77c79afcc97f49fa233c9000b15bd68cd4e77025
Poom Pianpak <ppianpak@gmail.com>
2018-05-01 13:31:45 -0600

    Make CPA+ fully works with Rosbridge and update gitignore

--- urs_wearable/cpa/cpa/main.cpp
+++ urs_wearable/cpa/cpa/main.cpp
@@ -14,79 +14,102 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#include <rosbridge_ws_client.hpp>
+#include <urs_wearable/GetPlan.h>
+
 #define VERSION "1.2"
 
 Reader reader;
 Timer timer;
 
-int main(int argc, char **argv)
+RosbridgeWsClient rbc("localhost:9090");
+std::string domainDef;
+
+void advertiseServiceCallback(std::shared_ptr<WsClient::Connection> /*connection*/, std::shared_ptr<WsClient::Message> message)
 {
-  std::cout << "CPA+ version " << VERSION << std::endl;
+  // message->string() is destructive, so we have to buffer it first
+  std::string messagebuf = message->string();
 
-  // Read our domain definition from PDDL file
-  std::ifstream ursDomainPDDL("urs_domain.pddl");
-  std::stringstream strStream;
-  strStream << ursDomainPDDL.rdbuf();
-  std::string domainDef = strStream.str();
+  std::cout << "Planning Request: " << messagebuf << std::endl;
 
-  ////////////////////
-//  std::cout << "planningRequest: " << std::endl << planningRequest.DebugString();
+  rapidjson::Document document;
+  if (document.Parse(messagebuf.c_str()).HasParseError())
+  {
+    std::cerr << "Error in parsing service request message: " << messagebuf << std::endl;
+    return;
+  }
 
+  // Define a set of objects for each type we have in domain definition
+  Objects uavIdObjects("uav_id");
+  Objects wpIdObjects("wp_id");
+
+  // Construct initial state definition
   std::string initDef("");
   int nInit = 0;
+
+  const rapidjson::Value& initialState = document["args"]["initial_state"];
+  const rapidjson::Value& initialStateAt = initialState["at"];
+  for (rapidjson::SizeType i = 0; i < initialStateAt.Size(); i++)
+  {
+    int uavId = initialStateAt[i]["uav_id"].GetUint();
+    int wpId = initialStateAt[i]["wp_id"].GetUint();
+
+    uavIdObjects.insert(uavId);
+    wpIdObjects.insert(wpId);
+
+    initDef += "(at " + uavIdObjects.intToObj(uavId) + " " + wpIdObjects.intToObj(wpId) + ")";
+  }
+  nInit += initialStateAt.Size();
+
+  // Construct goal state definition
   std::string goalDef("");
   int nGoal = 0;
 
-  // Define a set of objects for each type we have in domain definition
-  Objects uav_id("uav_id");
-  Objects wp_id("wp_id");
+  const rapidjson::Value& goalState = document["args"]["goal_state"];
+  const rapidjson::Value& goalStateAt = goalState["at"];
+  for (rapidjson::SizeType i = 0; i < goalStateAt.Size(); i++)
+  {
+    int uavId = goalStateAt[i]["uav_id"].GetUint();
+    int wpId = goalStateAt[i]["wp_id"].GetUint();
 
-//  pb_urs::State* initialState = planningRequest.mutable_initial();
-//  nInit += initialState->at_size();
-//  for (int i = 0; i < initialState->at_size(); i++) {
-//    const pb_urs::At& at = initialState->at(i);
-//
-//    uav_id.insert(at.uav_id());
-//    wp_id.insert(at.wp_id());
-//    initDef += "(at " + uav_id.intToObj(at.uav_id()) + " " + wp_id.intToObj(at.wp_id()) + ")";
-//  }
-//
-//  pb_urs::State* goalState = planningRequest.mutable_goal();
-//  nGoal += goalState->at_size();
-//  for (int i = 0; i < goalState->at_size(); i++) {
-//    const pb_urs::At& at = goalState->at(i);
-//
-//    uav_id.insert(at.uav_id());
-//    wp_id.insert(at.wp_id());
-//    goalDef += "(at " + uav_id.intToObj(at.uav_id()) + " " + wp_id.intToObj(at.wp_id()) + ")";
-//  }
+    uavIdObjects.insert(uavId);
+    wpIdObjects.insert(wpId);
 
+    goalDef += "(at " + uavIdObjects.intToObj(uavId) + " " + wpIdObjects.intToObj(wpId) + ")";
+  }
+  nGoal += goalStateAt.Size();
+
+  // Construct object definition
   std::string problemDef =
     "(define (problem urs_prob)\n"
     "  (:domain urs)\n"
     "  (:objects";
 
-  for (auto obj : uav_id.objs)
+  for (auto obj : uavIdObjects.objs)
   {
     problemDef += " " + obj;
   }
-  problemDef += " - " + uav_id.type;
+  problemDef += " - " + uavIdObjects.type;
 
-  for (auto obj : wp_id.objs)
+  for (auto obj : wpIdObjects.objs)
   {
     problemDef += " " + obj;
   }
-  problemDef += " - " + wp_id.type;
+  problemDef += " - " + wpIdObjects.type;
 
   problemDef += ")\n";
-  if (nInit > 1)
-  {
-    problemDef += "  (:init (and " + initDef + "))\n";
-  }
-  else if (nInit > 0)
-  {
-    problemDef += "  (:init " + initDef + ")\n";
-  }
+
+  // TODO: There is a bug in CPA that :init must have "and"
+//  if (nInit > 1)
+//  {
+//    problemDef += "  (:init (and " + initDef + "))\n";
+//  }
+//  else if (nInit > 0)
+//  {
+//    problemDef += "  (:init " + initDef + ")\n";
+//  }
+  problemDef += "  (:init (and " + initDef + "))\n";
+
   if (nGoal > 1)
   {
     problemDef += "  (:goal (and " + goalDef + "))\n";
@@ -110,6 +133,7 @@ int main(int argc, char **argv)
   Planner planner(&reader, &timer);
 
   FILE* f = fopen("theory_names", "r");
+
   // TODO: check open file
   char filename[16];
   while (!feof(f))
@@ -127,29 +151,63 @@ int main(int argc, char **argv)
   /**************************************/
   /* Return a plan to Execution Monitor */
   /**************************************/
-//  pb_urs::PlanningResponse planningResponse;
-//  for(map<string,int>::iterator it = planner.my_action_map.begin(); it != planner.my_action_map.end(); it++)
-//  {
-//    std::vector<std::string> tokens;
-//    boost::char_separator<char> sep {",()"};
-//    boost::tokenizer<boost::char_separator<char>> tok {it->first, sep};
-//    for (const auto &token : tok)
-//    {
-//      tokens.push_back((std::string)token);
-//      tokens.back().erase(0, 4);  // remove the leading "cpa_"
-//    }
-//
-//    pb_urs::Action* action = planningResponse.add_actions();
-//    if (tokens[0].compare("goto") == 0)
-//    {
-//      action->set_type(pb_urs::Action_ActionType_GOTO);
-//      pb_urs::Goto* action_goto = action->mutable_goto_();
-//
-//      action_goto->set_uav_id(uav_id.objToInt(tokens[1]));
-//      action_goto->set_wp_id(wp_id.objToInt(tokens[3]));
-//    }
-//  }
-//
-//  std::cout << "planningResponse: " << std::endl << planningResponse.DebugString();
-//  writeDelimitedToSockFD(execMonitorSockFD, planningResponse);
+  // Construct planning response message
+  rapidjson::Document values(rapidjson::kObjectType);
+  rapidjson::Document::AllocatorType& allocator = values.GetAllocator();
+
+  rapidjson::Value actions(rapidjson::kArrayType);
+
+  for(map<string, int>::iterator it = planner.my_action_map.begin(); it != planner.my_action_map.end(); it++)
+  {
+    std::cout << it->first << std::endl;
+
+    std::vector<std::string> tokens;
+    boost::char_separator<char> sep {",()"};
+    boost::tokenizer<boost::char_separator<char>> tok {it->first, sep};
+    for (const auto &token : tok)
+    {
+      tokens.push_back((std::string)token);
+      tokens.back().erase(0, 4);  // remove the leading "cpa_"
+    }
+
+    if (tokens[0].compare("goto") == 0)
+    {
+      rapidjson::Value actionGoto(rapidjson::kObjectType);
+      actionGoto.AddMember("uav_id", uavIdObjects.objToInt(tokens[1]), allocator);
+      actionGoto.AddMember("wp_id", wpIdObjects.objToInt(tokens[3]), allocator);
+
+      rapidjson::Value action(rapidjson::kObjectType);
+      action.AddMember("action_type", 0, allocator);
+      action.AddMember("action_goto", actionGoto, allocator);
+
+      actions.PushBack(action, allocator);
+    }
+  }
+
+  values.AddMember("actions", actions, allocator);
+
+  // Print planning response message
+  rapidjson::StringBuffer strbuf;
+  rapidjson::Writer<rapidjson::StringBuffer> writer(strbuf);
+  values.Accept(writer);
+  std::cout << "Planning Response: " << strbuf.GetString() << std::endl;
+
+  // Send planning response message
+  rbc.serviceResponse(document["service"].GetString(), document["id"].GetString(), true, values);
+}
+
+int main(int argc, char **argv)
+{
+  std::cout << "CPA+ version " << VERSION << std::endl;
+
+  // Read our domain definition from PDDL file
+  std::ifstream ursDomainPDDL("../urs_domain.pddl");
+  std::stringstream strStream;
+  strStream << ursDomainPDDL.rdbuf();
+  domainDef = strStream.str();
+
+  rbc.addClient("cpa_service_advertiser");
+  rbc.advertiseService("cpa_service_advertiser", "/cpa/get_plan", "urs_wearable/GetPlan", advertiseServiceCallback);
+
+  while(true);
 }
--- urs_wearable/cpa/cpa/swi_script.sh
+++ urs_wearable/cpa/cpa/swi_script.sh
@@ -2,7 +2,7 @@
 
 ../pddl2a/cpa.pddl2pl urs.pddl
 
-cat ../experiments/mult5zswi.pl pddl2pl.pl > new.pl
+cat ../mult5zswi.pl pddl2pl.pl > new.pl
 swipl -L128m -G128m -f new.pl -g 'main,halt.' > trash
 
 #../cpa/cpa+ theory_names > temp
