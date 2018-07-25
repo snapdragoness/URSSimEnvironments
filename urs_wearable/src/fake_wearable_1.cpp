8d72d7bd0a72c17828acef76bf467f0047e43cf1
Poom Pianpak <ppianpak@gmail.com>
2018-07-25 14:47:16 -0600

    Add some tests in fake_wearable

--- urs_wearable/src/fake_wearable_1.cpp
+++ urs_wearable/src/fake_wearable_1.cpp
@@ -6,6 +6,8 @@
 #include <ros/ros.h>
 
 #include "colormod.h"
+#include "urs_wearable/LocationAdd.h"
+#include "urs_wearable/location_table.h"
 #include "urs_wearable/SetGoal.h"
 
 const std::string SET_GOAL_SERVICE_NAME = "/urs_wearable/set_goal";
@@ -16,15 +18,53 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "fake_wearable");
   ros::NodeHandle nh;
 
-  urs_wearable::SetGoal set_goal_srv;
+  LocationTable::location_id_type location_id_sw, location_id_ne;
+  urs_wearable::PoseEuler pose_sw, pose_ne;
+  pose_sw.position.x = -20;
+  pose_sw.position.y = -20;
+  pose_sw.position.z = 0;
+  pose_ne.position.x = 20;
+  pose_ne.position.y = 20;
+  pose_ne.position.z = 20;
 
+  ros::ServiceClient location_add_client = nh.serviceClient<urs_wearable::LocationAdd>("/urs_wearable/location_add");
+  urs_wearable::LocationAdd location_add_srv;
+
+  location_add_srv.request.pose = pose_sw;
+  if (location_add_client.call(location_add_srv))
+  {
+    location_id_sw = location_add_srv.response.location_id;
+  }
+  else
+  {
+    ROS_ERROR("Call /urs_wearable/location_add error");
+  }
+
+  location_add_srv.request.pose = pose_ne;
+  if (location_add_client.call(location_add_srv))
+  {
+    location_id_ne = location_add_srv.response.location_id;
+  }
+  else
+  {
+    ROS_ERROR("Call /urs_wearable/location_add error");
+  }
+
+  urs_wearable::SetGoal set_goal_srv;
   urs_wearable::Predicate pred;
+  pred.type = urs_wearable::Predicate::TYPE_ACTIVE_REGION;
+  pred.predicate_active_region.location_id_sw.value = location_id_sw;
+  pred.predicate_active_region.location_id_ne.value = location_id_ne;
+  pred.predicate_active_region.truth_value = true;
+  set_goal_srv.request.goal.push_back(pred);
+
   pred.type = urs_wearable::Predicate::TYPE_TOOK_OFF;
-  pred.predicate_took_off.truth_value = true;
   pred.predicate_took_off.drone_id.value = 0;
+  pred.predicate_took_off.truth_value = true;
   set_goal_srv.request.goal.push_back(pred);
 
   pred.predicate_took_off.drone_id.value = 1;
+  pred.predicate_took_off.truth_value = true;
   set_goal_srv.request.goal.push_back(pred);
 
   if (ros::service::call(SET_GOAL_SERVICE_NAME, set_goal_srv))
